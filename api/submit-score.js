1df657a06d8dfc7d8795c777eadcb59e6c68efbd
Tomtaru <81351213+T0mt4ru@users.noreply.github.com>
2025-07-02 00:27:59 +0200

    Update submit-score.js

--- api/submit-score.js
+++ api/submit-score.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import Filter from 'bad-words'; // Correcte import voor bad-words
+import { Filter } from 'bad-words'; // Correcte import voor bad-words
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
